b842e1a28c296677df5a71f3f77273930d2097a7
AlexKaravaev <alexkaravev@gmail.com>
2019-05-11 11:34:46 +0300

    Initial commit

---
+++ include/raw_udp_receiver/udp_client.h
@@ -0,0 +1,40 @@
+#ifndef UDP_CLIENT_H
+#define UDP_CLIENT_H
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string>
+#include <netinet/in.h>
+#include <ros/ros.h>
+
+/** Base input class for socket/playback listening**/
+
+class Input
+{
+    public:
+        Input(ros::NodeHandle nh);
+        virtual ~Input(){}
+        // Get packet from socket/topic and publish it to another topic 
+        virtual void getPacket();
+
+    private:
+        ros::NodeHandle nh_;
+        uint16_t port_;
+        std::string device_ip_;
+         
+};
+
+class Input_Socket: Input
+{
+    private:
+        int sockfd_;
+        in_addr device_ip_;
+};
+
+class Input_Topic: Input
+{
+    private:
+        std::string pub_topic_name_;
+};
+#endif
---
+++ src/udp_receiver_node.cpp
@@ -0,0 +1,1 @@
+#include <raw_udp_receiver/udp_client>
